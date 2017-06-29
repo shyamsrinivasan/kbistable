1aa24049aca11d306fdbb947cdb5ae2dc249d2a9
shyamsrinivasan <shyamsweth@gmail.com>
2017-06-29 10:09:30 -0400

    change uptake rate and code paths

--- kotte-bistable/Kotte2014/figureDrivers/stoich_kmss.m
+++ kotte-bistable/Kotte2014/figureDrivers/stoich_kmss.m
@@ -2,7 +2,6 @@
 % states
 % change uptake rates on line 16 (1 or 2 a.u.) to correct envelopes
 % build stoichioemtrc matrices
-addpath(genpath('C:\Users\shyam\Documents\Courses\CHE1125Project\IntegratedModels\KineticModel'));
 rxfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\kbistable\kotte-bistable\Kotte2014\Kotte2014.txt';
 cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\kbistable\kotte-bistable\Kotte2014\Kotte2014C.txt';
 
@@ -13,7 +12,7 @@ cnfname = 'C:\Users\shyam\Documents\Courses\CHE1125Project\kbistable\kotte-bista
 [mc,FBAmodel,met] = readCNCfromFile(cnfname,FBAmodel);
 
 % run FBA
-Vup_struct.ACt2r = 1;
+Vup_struct.ACt2r = 2;
 Vup_struct.ENZ1ex = 1;
 FBAmodel = FBAfluxes(FBAmodel,'fba',{'ACt2r','ENZ1ex'},Vup_struct,...
                     [find(strcmpi(FBAmodel.rxns,'FDex'))...
